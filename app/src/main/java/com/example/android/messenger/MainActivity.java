bcdf991fbc52937e7d683e4be5541fd656313703
Vikas <vikasford@gmail.com>
2018-03-01 15:06:46 +0530

    commit

---
+++ app/src/main/java/com/example/android/messenger/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.messenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Call onSendMessage() method when the button is clicked
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+//        Intent intent = new Intent(this,ReceiveMessageActivity.class);
+//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
+    }
+}
